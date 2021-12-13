08b048731a883dccad2184f6506e8af644e397f1
Chris <cochsenreither@protonmail.com>
2021-12-13 11:31:25 -0700

    check session storage for username, chatId

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,4 +2,23 @@ const sock = new SockJS('https://qw3rt.ochsec1.repl.co/chat')
 
 const message = document.getElementById('message')
 const sendBtn = document.getElementById('send-btn')
+let username
+let chatId
 
+function checkForSessionVariables() {
+    const urlSplit = window.location.href.split('/')
+    const chatIdFromUrl = urlSplit[urlSplit.length - 1]
+    username = sessionStorage.getItem('username')
+    chatId = sessionStorage.getItem('chatId')
+
+
+    if (!username) {
+        window.location.href = '/'
+    }
+
+    if (!chatId || (chatId !== chatIdFromUrl)) {
+        window.location.href = '/'
+    }
+}
+
+document.addEventListener('DOMContentLoaded', checkForSessionVariables)
